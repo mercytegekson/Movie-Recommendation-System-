97ae5c334e4999ef492eaafa02ebc9b9e082628e
dev-ezzy <bettezra07@gmail.com>
2024-01-14 15:27:33 +0300

    commit

--- functions.py
+++ functions.py
@@ -24,3 +24,18 @@ def sns_ycount(column , data):
     sns.countplot(y = column, data = data)
     plt.title(f"{column} count in our data set")
     plt.show();
+    
+    
+def plotting_trends(data, column):
+    #set rating_timestamp as the index
+    #set rating_timestamp as the index
+    trends_data = data.set_index("column")
+    #yearly resampling 
+    yearly_trend = trends_data.resample("Y").size()
+    #plotting the trend
+    plt.figure(figsize= (15, 6))
+    yearly_trend.plot(marker = "o", linestyle = "--", color = "red")
+    plt.title("The trend of ratings yearly over the years")
+    plt.xlabel("rating_timestamp")
+    plt.ylabel("count")
+    plt.show();
